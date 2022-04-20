f146036717518e9d767532ff9443c10896ed508a
kong <king@qq.com>
2022-04-20 16:47:46 +0800

    commit

--- encrypt/springboot-mybatisplus-encrypt/src/main/java/com/kq/mybatisplus/enums/DesensitionType.java
+++ encrypt/springboot-mybatisplus-encrypt/src/main/java/com/kq/mybatisplus/enums/DesensitionType.java
@@ -12,7 +12,8 @@ public enum DesensitionType {
     ID_CARD("idCard", "16或者18身份证号", "^(\\d{4})\\d{8,10}(\\w{4})$", "$1****$2"),
     BANK_CARD("bankCardNo", "银行卡号", "^(\\d{4})\\d*(\\d{4})$", "$1****$2"),
     REAL_NAME("realName","真实姓名","(?<=.{1}).*(?=.{1})","*"),
-    EMAIL("email","电子邮箱","(\\w+)\\w{5}@(\\w+)", "$1***@$2"),
+//    EMAIL("email","电子邮箱","(\\w+)\\w{5}@(\\w+)", "$1***@$2"),
+    EMAIL("email","电子邮箱","(\\w+)\\w{3}@(\\w+)(\\.[\\w-]+)", "$1***@$2"),
     CUSTOM("custom", "自定义正则处理", ""),
     TRUNCATE("truncate", "字符串截取处理", ""),
     ;
